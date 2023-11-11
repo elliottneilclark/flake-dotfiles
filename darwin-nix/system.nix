177d15b486095ae8f0e1658ef48e85c9a7dfd0ef
Elliott Clark <elliott@batteriesincl.com>
2023-11-11 16:39:44 -0600

    feat: jj config
    Summary:
    jj template
    
    Test Plan:
    It worked

--- darwin-nix/system.nix
+++ darwin-nix/system.nix
@@ -2,15 +2,33 @@
 
   system = {
     defaults = {
+      # login window settings
+      loginwindow = {
+        # disable guest account
+        GuestEnabled = false;
+        # show name instead of username
+        SHOWFULLNAME = false;
+      };
+
       NSGlobalDomain = {
+        "com.apple.sound.beep.feedback" = 0;
+        "com.apple.sound.beep.volume" = 0.0;
         AppleInterfaceStyle = "Dark";
         # I'm ok with longer filenames
         AppleShowAllExtensions = true;
         AppleShowScrollBars = "Automatic";
+        # Dont allow press and hold since it stops
+        # key repeat
+        ApplePressAndHoldEnabled = false;
         # How fast the keypress repeats
         InitialKeyRepeat = 15;
         KeyRepeat = 2;
         # Don't help me
+        # Disable autocorrect
+        NSAutomaticCapitalizationEnabled = false;
+        NSAutomaticDashSubstitutionEnabled = false;
+        NSAutomaticPeriodSubstitutionEnabled = false;
+        NSAutomaticQuoteSubstitutionEnabled = false;
         NSAutomaticSpellingCorrectionEnabled = false;
       };
       dock = {
